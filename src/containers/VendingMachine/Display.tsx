cf0b1050d7e955200123fd22201e212fcd99ef87
yusufdeepwork <yusuf.deepwork@gmail.com>
2024-11-13 13:38:47 +0300

    Modularize display component

--- src/containers/VendingMachine/Display.tsx
+++ src/containers/VendingMachine/Display.tsx
@@ -13,6 +13,14 @@ const Display = ({ isMaximumEnergyExceed }: DisplayProps) => {
   )
 
   const renderText = () => {
+    const isRefunded = status === 'refundedMoney'
+    const isOperationCompleted = status === 'completedOperation'
+    const isNeedMoney = totalMoney === 0 && selectedProduct
+    const canAddMoney = totalMoney > 0 && selectedProduct
+    const refundAmount = canAddMoney
+      ? totalMoney - Number(selectedProduct?.price)
+      : 0
+
     if (isMaximumEnergyExceed) {
       return (
         <p className="text-red-500 text-center break-words whitespace-pre-wrap">
@@ -20,13 +28,14 @@ const Display = ({ isMaximumEnergyExceed }: DisplayProps) => {
         </p>
       )
     }
-    if (status === 'refundedMoney') {
+
+    if (isRefunded) {
       return (
         <p className="text-green-500">{Texts.REFUND_MESSAGE(refundedMoney)}</p>
       )
     }
 
-    if (status === 'completedOperation') {
+    if (isOperationCompleted) {
       return (
         <div className="text-green-500">
           {Texts.OPERATION_COMPLETED}
@@ -36,36 +45,41 @@ const Display = ({ isMaximumEnergyExceed }: DisplayProps) => {
       )
     }
 
-    if (totalMoney === 0 && !!selectedProduct) return <p>{Texts.NEED_MONEY}</p>
+    if (isNeedMoney) {
+      return <p>{Texts.NEED_MONEY}</p>
+    }
 
-    const refundAmount = totalMoney - Number(selectedProduct?.price)
-
-    return (
-      totalMoney > 0 &&
-      !!selectedProduct && (
+    if (canAddMoney) {
+      return (
         <>
           <p>
             {Texts.ADD_MONEY_PROMPT} {formatMoney(totalMoney)}
           </p>
-          <p>
-            {refundAmount > 0
-              ? Texts.OPERATION_COMPLETED_REFUND_MESSAGE(refundAmount)
-              : null}
-          </p>
+          {refundAmount > 0 && (
+            <p>{Texts.OPERATION_COMPLETED_REFUND_MESSAGE(refundAmount)}</p>
+          )}
         </>
       )
+    }
+
+    return null
+  }
+
+  const renderMainText = () => {
+    if (isMaximumEnergyExceed) return null
+
+    return (
+      <p>
+        {selectedProduct
+          ? Texts.SELECTED_PRODUCT_PROMPT(selectedProduct.name)
+          : status !== 'completedOperation' && Texts.SELECT_PRODUCT_PROMPT}
+      </p>
     )
   }
 
   return (
     <div className="flex w-full max-w-80 bg-gray-800 text-white p-4 md:h-20 h-auto min-h-20 items-center justify-center flex-col rounded-lg text-center">
-      {!isMaximumEnergyExceed && (
-        <p>
-          {selectedProduct
-            ? Texts.SELECTED_PRODUCT_PROMPT(selectedProduct.name)
-            : status !== 'completedOperation' && Texts.SELECT_PRODUCT_PROMPT}
-        </p>
-      )}
+      {renderMainText()}
       {renderText()}
     </div>
   )
